57ae88fa9daf5c69d573a026af1d3bc64d978291
kian <kheus@tudelft.nl>
2024-09-26 14:03:44 +0200

    More changes to prep for the AutoEncoder (dun dun duuuuun)

---
+++ Kian_code/find_A_theta.py
@@ -0,0 +1,56 @@
+from find_eoms import *
+import sympy as sp
+from sympy.printing.str import StrPrinter
+
+
+# Actuator parameters
+xa, ya = sp.symbols('xa ya') #x, y coordinate of actuator
+
+# Rope connection length along joint 2
+lr2 = sp.symbols('lr2')
+kx = l1 * sp.cos(q1) + lr2 * sp.cos(q2)
+ky = l1 * sp.sin(q1) + lr2 * sp.sin(q2)
+
+k = sp.Matrix([[kx],
+               [ky]])
+
+dkdq1 = sp.diff(k, q1)
+dkdq2 = sp.diff(k, q2)
+
+R = sp.Matrix([[xa - kx],
+               [ya - ky]])
+Rx = R[0]
+Ry = R[1]
+r = R/R.norm()
+
+l = sp.sqrt((xa - kx)**2 + (ya - ky)**2)
+A1 = sp.diff(l, q1).simplify()
+A2 = sp.diff(l, q2).simplify()
+sp.pprint(A1, wrap_line = False)
+sp.pprint(A2, wrap_line = False)
+
+A = sp.Matrix([[A1],
+               [A2]])
+
+A = sp.simplify(A)
+print(custom_pretty(A))
+print("\n")
+sp.pprint(A, wrap_line=False)
+
+
+h1 = sp.atan2(Rx, Ry)
+h1 = l
+h2 = q1
+theta = sp.Matrix([[h1],
+                   [h2]])
+
+Jh = theta.jacobian(q)
+Jh = sp.simplify(Jh)
+sp.pprint(Jh, wrap_line = False)
+
+Jh_inv = Jh.inv()
+Jh_invtrans = Jh_inv.transpose()
+
+A_theta = Jh_invtrans * A
+A_theta = sp.simplify(A_theta)
+sp.pprint(A_theta, wrap_line = False)
