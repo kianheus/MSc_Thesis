4bb06a7fb3f4b88627778f60c7e5d2b20a36d1bc
kian <kheus@tudelft.nl>
2024-11-07 19:10:01 +0100

    Add actuator location to robot parameters dict

--- Kian_code/Double_Pendulum/Lumped_Mass/robot_parameters.py
+++ Kian_code/Double_Pendulum/Lumped_Mass/robot_parameters.py
@@ -3,4 +3,6 @@ LUMPED_PARAMETERS = dict(
     l2 = 2, #Length of link 2 [m]
     m = 3, #System mass [kg] (attached at the end of link 2)
     g = 9.81, #Gravitational acceleration [m/s^2]
+    xa = 5, #actuator rope attachment point x-location [m]
+    ya = 1, #actuator rope attachment point y-location [m]
 )
