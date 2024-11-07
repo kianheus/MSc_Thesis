feb4f8af9b267cc6f8eca8fcfa5a2b365c06846f
kian <kheus@tudelft.nl>
2024-11-07 19:10:33 +0100

    Add analytica calculation of theta_1 and theta_2. Current code is inconsistent, some inputs expect a batch, others individual values. Look into torch.vmap for improvement

--- Kian_code/Double_Pendulum/Lumped_Mass/transforms.py
+++ Kian_code/Double_Pendulum/Lumped_Mass/transforms.py
@@ -16,3 +16,26 @@ def transform_dynamical_matrices(M_q: Tensor, C_q: Tensor, G_q: Tensor, J_h: Ten
     return M_theta, C_theta, G_theta
 
 
+def analytic_theta_1(rp: dict, q: Tensor) -> Tensor:
+    
+    # h1 is defined as the length between the actuator attachment point and the mass of the double pendulum
+    Rx = rp["xa"] - rp["l1"] * torch.cos(q[:, 0]) - rp["l2"] * torch.cos(q[:, 1])
+    Ry = rp["ya"] - rp["l1"] * torch.sin(q[:, 0]) - rp["l2"] * torch.sin(q[:, 1])
+    
+    h1 = torch.sqrt(Rx**2 + Ry**2)
+    
+    return h1
+    
+    
+def analytic_theta_2(rp: dict, q: Tensor) -> Tensor:
+    
+    # h2 is defined as the arctan between the vector from mass of double pendulum 
+    # to the actuator point. 
+    # This was Cosimo's hunch, and has been verified in the Mathematica code
+    
+    Rx = rp["xa"] - rp["l1"] * torch.cos(q[0]) - rp["l2"] * torch.cos(q[1])
+    Ry = rp["ya"] - rp["l1"] * torch.sin(q[0]) - rp["l2"] * torch.sin(q[1])    
+    
+    h2 = torch.atan2(Ry/Rx)
+    
+    return h2
