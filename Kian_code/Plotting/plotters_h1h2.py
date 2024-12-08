4225944fc67653f8cef86eda8ed51cb1dc7ea31a
kian <kheus@tudelft.nl>
2024-12-08 15:28:23 +0100

    Update neural network for learning h1h2, update plotter h1h2

--- Kian_code/Plotting/plotters_h1h2.py
+++ Kian_code/Plotting/plotters_h1h2.py
@@ -289,3 +289,150 @@ def plot_decoupling_inv(model, device, rp, epoch):
     plot_3d_double(q1_grid_np, q2_grid_np, M_th_ratio_grid_np_capped, M_th_ratio_grid_np, 
                    plot_title, title_1, title_2, xlabel, ylabel, zlabel, z_limits)
     
+
+def plot_decoupling(model, device, rp, epoch):
+    
+    #Obtain [q1,q1]-grid and flatten
+    q1_grid, q2_grid = plot_data()
+    q_grid_flat = torch.stack([q1_grid.flatten(), q2_grid.flatten()], dim=-1).to(device)  # Shape: (N, 2)
+    
+    theta, J_h, q_hat, J_h_ana = model(q_grid_flat)
+
+    J_h_inv = J_h.transpose(1,2).to(device)
+    J_h_inv_trans = J_h.to(device)
+
+
+    matrices_vmap = torch.vmap(dynamics.dynamical_matrices, 
+                                   in_dims=(None, 0, 0))
+
+    
+    # Inputting q as q_d. This is not pretty but it doesn't matter since we only care about M_q
+    M_q, C_q, G_q = matrices_vmap(rp, q_grid_flat, q_grid_flat)
+    
+        
+    M_th, C_th, G_th = transforms.transform_dynamical_matrices(M_q, C_q, G_q, J_h, device)
+    M_th_ana, C_th_ana, G_th_ana = transforms.transform_dynamical_matrices(M_q, C_q, G_q, J_h_ana, device)
+
+
+    matrices_th_ana_vmap = torch.vmap(dynamics.dynamical_matrices_th, 
+                                   in_dims=(None, 0, 0))
+    M_th_ana, C_th_ana, G_th_ana = matrices_th_ana_vmap(rp, q_grid_flat, q_grid_flat)
+    
+    off_dia = M_th[:, 0, 1]
+    diag_elements = M_th_ana[:, [0, 1], [0, 1]]
+    diag_product = torch.sqrt(diag_elements[:, 0] * diag_elements[:, 1])
+    try:
+        M_th_ratio = off_dia/diag_product
+    except:
+        print("diag_product is zero")
+        diag_product += 1e-8
+        M_th_ratio = off_dia/diag_product
+
+
+    off_dia_ana = M_th_ana[:, 0, 1]
+    diag_elements_ana = M_th_ana[:, [0, 1], [0, 1]] 
+    diag_product_ana = torch.sqrt(diag_elements_ana[:, 0] * diag_elements_ana[:, 1])
+
+    try:
+        M_th_ratio_ana = off_dia_ana/diag_product_ana
+    except:
+        print("diag_product_ana is zero")
+        diag_product_ana += 1e-8
+        M_th_ratio_ana = off_dia_ana/diag_product_ana
+    for index, sub_tensor in enumerate(M_th_ana):
+        if M_th_ratio_ana[index] > 0.1:
+            print("M_th_ratio_ana > 0.1:", M_th_ratio_ana[index])
+            print("q:", q_grid_flat[index])
+            print("M_th_ana:\n", sub_tensor)
+            print("J_h_ana:\n", J_h_ana[index])
+            break
+
+
+    M_th_ratio_grid = M_th_ratio.view(q1_grid.shape)
+    M_th_ratio_grid_np = np.abs(M_th_ratio_grid.detach().cpu().numpy())
+    M_th_ratio_grid_np_capped = np.copy(M_th_ratio_grid_np)
+    M_th_ratio_grid_np_capped[M_th_ratio_grid_np_capped >= 1] = 1
+    
+    M_th_ratio_ana_grid = M_th_ratio_ana.view(q1_grid.shape)
+    M_th_ratio_ana_grid_np = np.abs(M_th_ratio_ana_grid.detach().cpu().numpy())
+    q1_grid_np = q1_grid.numpy()
+    q2_grid_np = q2_grid.numpy()
+    
+    plot_title = "Ratio of off-diagonal to diagonal entries" + str(epoch + 1)
+    title_1 = r"Learned $M_{theta}$ ratio (capped)"
+    title_2 = r"Learned $M_{theta}$ ratio (full)"
+    xlabel = "$q_1$ (rad)"
+    ylabel = "$q_2$ (rad)"
+    zlabel = "$Ratio$ (-)"
+    z_limits = (0, 1)
+    
+    plot_3d_double(q1_grid_np, q2_grid_np, M_th_ratio_grid_np_capped, M_th_ratio_grid_np, 
+                   plot_title, title_1, title_2, xlabel, ylabel, zlabel, z_limits)
+    
+
+def plot_input_decoupling(model, device, rp, epoch):
+    
+    #Obtain [q1,q1]-grid and flatten
+    q1_grid, q2_grid = plot_data()
+    q_grid_flat = torch.stack([q1_grid.flatten(), q2_grid.flatten()], dim=-1).to(device)  # Shape: (N, 2)
+    
+    theta, J_h, q_hat, J_h_ana = model(q_grid_flat)
+
+    input_matrix_vmap = torch.vmap(dynamics.input_matrix, in_dims=(None, 0))
+    A_q = input_matrix_vmap(rp, q_grid_flat)
+    A_th = transforms.transform_input_matrix(A_q, J_h, device)
+
+    # Extract actuated and unactuated part of input matrix
+    A_th_a = A_th[:, 0]
+    A_th_u = A_th[:, 1]
+
+    A_th_a_grid = A_th_a.view(q1_grid.shape)
+    A_th_a_grid_np = A_th_a_grid.detach().cpu().numpy()
+
+    A_th_u_grid = A_th_u.view(q1_grid.shape)
+    A_th_u_grid_np = A_th_u_grid.detach().cpu().numpy()
+    
+    q1_grid_np = q1_grid.numpy()
+    q2_grid_np = q2_grid.numpy()
+    
+    plot_title = "Value of A1 and A2 of input matrix" + str(epoch + 1)
+    title_1 = r"Learned A1"
+    title_2 = r"Learned A2"
+    xlabel = "$q_1$ (rad)"
+    ylabel = "$q_2$ (rad)"
+    zlabel = "Input (-)"
+    
+    plot_3d_double(q1_grid_np, q2_grid_np, A_th_a_grid_np, A_th_u_grid_np, 
+                   plot_title, title_1, title_2, xlabel, ylabel, zlabel)
+    
+    
+def plot_J_h_unitary(model, device, rp, epoch):
+    
+    #Obtain [q1,q1]-grid and flatten
+    q1_grid, q2_grid = plot_data()
+    q_grid_flat = torch.stack([q1_grid.flatten(), q2_grid.flatten()], dim=-1).to(device)  # Shape: (N, 2)
+    
+    theta, J_h, q_hat, J_h_ana = model(q_grid_flat)
+
+    J_h_trans = J_h.transpose(-2,-1)
+
+    dot_product = torch.bmm(J_h_trans, J_h)
+
+    big_identity = torch.eye(2).expand(len(q_grid_flat), -1, -1).to(device)
+
+    criterion = torch.nn.MSELoss(reduction='none')
+    elementwise_loss = criterion(dot_product, big_identity)
+    loss_J_h_unitary = elementwise_loss.mean(dim=(-2, -1))
+
+    loss_J_h_unitary_grid = loss_J_h_unitary.view(q1_grid.shape)
+    loss_J_h_unitary_grid_np = loss_J_h_unitary_grid.detach().cpu().numpy()
+
+    q1_grid_np = q1_grid.numpy()
+    q2_grid_np = q2_grid.numpy()
+    
+    plot_title = "J_h unitary loss"
+    xlabel = "$q_1$ (rad)"
+    ylabel = "$q_2$ (rad)"
+    zlabel = "Loss (-)"
+    
+    plot_3d(q1_grid_np, q2_grid_np, loss_J_h_unitary_grid_np, plot_title, xlabel, ylabel, zlabel)
