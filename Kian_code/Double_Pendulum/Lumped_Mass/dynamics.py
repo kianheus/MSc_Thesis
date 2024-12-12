Fix error in calculation of input matrix

Kian_code/Double_Pendulum/Lumped_Mass/dynamics.py
@@ -19,10 +19,7 @@ def dynamical_matrices(rp: dict, q: Tensor, q_d: Tensor) -> Tuple[Tensor, Tensor
         C_q: coriolis and centrifugal matrix of shape (2, 2)
         G_q: gravitational matrix of shape (2, )     
     """
-    
-
-    
-    
+       
     c1 = torch.cos(q[0]).unsqueeze(0)
     c2 = torch.cos(q[1]).unsqueeze(0)
     s12 = torch.cos(q[0]-q[1]).unsqueeze(0)
@@ -45,28 +42,6 @@ def dynamical_matrices(rp: dict, q: Tensor, q_d: Tensor) -> Tuple[Tensor, Tensor
 
 
     return M_q, C_q, G_q 
-    
-    """
-
-    M_q = torch.tensor(
-        [[rp["l1"]**2 * rp["m"],                    rp["l1"] * rp["l2"] * rp["m"] * c12],
-         [rp["l1"] * rp["l2"] * rp["m"] * c12,      rp["l2"]**2 * rp["m"]]]
-         )
-
-    C_q = torch.tensor(
-        [[0,                                                  rp["l1"] * rp["l2"] * rp["m"] * q_d[1] * s12],
-         [-rp["l1"] * rp["l2"] * rp["m"] * q_d[0] * s12,      0]]
-         )
-    
-    G_q = torch.tensor(
-        [[rp["g"] * rp["l1"] * rp["m"] * c1],
-         [rp["g"] * rp["l2"] * rp["m"] * c2]]
-    )
-    
-
-    return M_q, C_q, G_q 
-    """
-
 
 def dynamical_matrices_th(rp: dict, q: Tensor, q_d: Tensor) -> Tuple[Tensor, Tensor, Tensor]:
     """
@@ -104,19 +79,19 @@ def input_matrix(rp: dict, q: Tensor) -> Tensor:
     """
     Computes the input matrix A (analytically) in the equation 
     M_q @ q_dd + C_q @ q_d + G_q = A_q @ u
+
+    Args:
+        rp: dictionary of robot parameters
+        q: link angles of shape (2, )
+    Returns:
+        A_q: input matrix of shape (2, )   
     """
 
-    """
-    USE WOLFRAM MATHEMATICA TO DEFINE
-    """
-
-
-    
     Rx = rp["xa"] - rp["l1"] * torch.cos(q[0]) - rp["l2"] * torch.cos(q[1])
     Ry = rp["ya"] - rp["l1"] * torch.sin(q[0]) - rp["l2"] * torch.sin(q[1])
 
-    A_q1 = (rp["l1"] * torch.sin(q[0]) * Rx - rp["l1"] * torch.sin(q[0]) * Ry) / torch.sqrt(Rx**2 + Ry**2).unsqueeze(0).unsqueeze(0)
-    A_q2 = (rp["l2"] * torch.sin(q[1]) * Rx - rp["l2"] * torch.sin(q[1]) * Ry) / torch.sqrt(Rx**2 + Ry**2).unsqueeze(0).unsqueeze(0)
+    A_q1 = ( (rp["l1"] * torch.sin(q[0]) * Rx - rp["l1"] * torch.cos(q[0]) * Ry) / torch.sqrt(Rx**2 + Ry**2) ).unsqueeze(0).unsqueeze(0)
+    A_q2 = ( (rp["l2"] * torch.sin(q[1]) * Rx - rp["l2"] * torch.cos(q[1]) * Ry) / torch.sqrt(Rx**2 + Ry**2) ).unsqueeze(0).unsqueeze(0)
 
     A_q = torch.cat((A_q1, A_q2), dim = 0)
 