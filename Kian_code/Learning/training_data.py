d1c559cd32278eb61b588d333e18dfc16358fa7e
kian <kheus@tudelft.nl>
2024-12-12 13:48:08 +0100

    Change points to only contain q, not q_dot

--- Kian_code/Learning/training_data.py
+++ Kian_code/Learning/training_data.py
@@ -1,24 +1,25 @@
 import torch
 
 # Number of samples
-n_samples = 1000
+n_samples = 3000
 
 # Data range
-q1_low  = -torch.pi/4
-q1_high = torch.pi/4
-q2_low  = -torch.pi/4
-q2_high = torch.pi/4
-q1_d_low  = -torch.pi
-q1_d_high = torch.pi
-q2_d_low  = -torch.pi
-q2_d_high = torch.pi
+q1_low  = -torch.pi/2
+q1_high = torch.pi/2
+q2_low  = -torch.pi/2
+q2_high = torch.pi/2
+#q1_d_low  = -torch.pi
+#q1_d_high = torch.pi
+#q2_d_low  = -torch.pi
+#q2_d_high = torch.pi
 
 
 # Generate uniformly distributed points for q1 and q2
 q1 = q1_low + (q1_high - q1_low) * torch.rand(n_samples)
 q2 = q2_low + (q2_high - q2_low) * torch.rand(n_samples)
-q1_d = q1_d_low + (q1_d_high - q1_d_low) * torch.rand(n_samples)
-q2_d = q2_d_low + (q2_d_high - q2_d_low) * torch.rand(n_samples)
+#q1_d = q1_d_low + (q1_d_high - q1_d_low) * torch.rand(n_samples)
+#q2_d = q2_d_low + (q2_d_high - q2_d_low) * torch.rand(n_samples)
 
 # Stack q1 and q2 to get the 2D coordinates
-points = torch.stack([q1, q2, q1_d, q2_d], axis=1)
+points = torch.stack([q1, q2], axis=1)
+#points = torch.stack([q1, q2, q1_d, q2_d], axis=1)
