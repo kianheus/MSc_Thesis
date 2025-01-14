6d7670827ff4e7a9a18a760e8d16835ee4ead1ae
kianheus <Kianheus@gmail.com>
2025-01-14 16:06:58 +0100

    Remove derivative data and change data range

--- Kian_code/Learning/training_data.py
+++ Kian_code/Learning/training_data.py
@@ -4,21 +4,14 @@ import torch
 n_samples = 3000
 
 # Data range
-q1_low  = -torch.pi/2
-q1_high = torch.pi/2
-q2_low  = -torch.pi/2
-q2_high = torch.pi/2
-#q1_d_low  = -torch.pi
-#q1_d_high = torch.pi
-#q2_d_low  = -torch.pi
-#q2_d_high = torch.pi
-
+q1_low  = -torch.pi
+q1_high = torch.pi
+q2_low  = -torch.pi
+q2_high = torch.pi
 
 # Generate uniformly distributed points for q1 and q2
 q1 = q1_low + (q1_high - q1_low) * torch.rand(n_samples)
 q2 = q2_low + (q2_high - q2_low) * torch.rand(n_samples)
-#q1_d = q1_d_low + (q1_d_high - q1_d_low) * torch.rand(n_samples)
-#q2_d = q2_d_low + (q2_d_high - q2_d_low) * torch.rand(n_samples)
 
 # Stack q1 and q2 to get the 2D coordinates
 points = torch.stack([q1, q2], axis=1)
