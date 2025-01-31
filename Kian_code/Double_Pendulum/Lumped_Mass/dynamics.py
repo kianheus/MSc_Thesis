Fix calculation errors

Kian_code/Double_Pendulum/Lumped_Mass/dynamics.py
@@ -21,7 +21,7 @@ def dynamical_matrices(rp: dict, q: Tensor, q_d: Tensor) -> Tuple[Tensor, Tensor
     """
     c1 = torch.cos(q[0]).unsqueeze(0)
     c2 = torch.cos(q[1]).unsqueeze(0)
-    s12 = torch.cos(q[0]-q[1]).unsqueeze(0)
+    s12 = torch.sin(q[0]-q[1]).unsqueeze(0)
     c12 = torch.cos(q[0]-q[1]).unsqueeze(0)
     
     M_q_11 = torch.tensor([rp["l1"]**2 * rp["m"]]).to(device) 
@@ -36,7 +36,7 @@ def dynamical_matrices(rp: dict, q: Tensor, q_d: Tensor) -> Tuple[Tensor, Tensor
     C_q = torch.cat((C_q_1, C_q_2), dim = 0)
     
     G_q_1 = torch.tensor([rp["g"] * rp["l1"] * rp["m"]]).unsqueeze(0).to(device)  * c1.to(device)
-    G_q_2 = torch.tensor([rp["g"] * rp["l1"] * rp["m"]]).unsqueeze(0).to(device)  * c2.to(device)
+    G_q_2 = torch.tensor([rp["g"] * rp["l2"] * rp["m"]]).unsqueeze(0).to(device)  * c2.to(device)
     G_q = torch.cat((G_q_1, G_q_2), dim = 0)
 
 