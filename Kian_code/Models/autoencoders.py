5405ebdc11d1e6b3ba0088563ee079f86d1d3f43
kianheus <Kianheus@gmail.com>
2025-01-31 16:44:31 +0100

    Create file containing autoencoder structures, to be used by controller

---
+++ Kian_code/Models/autoencoders.py
@@ -0,0 +1,142 @@
+import torch
+import torch.nn as nn
+
+import Double_Pendulum.Lumped_Mass.transforms as transforms
+
+"""
+This file contains the architectures of a number of Autoencoders used in my thesis. 
+Currently it only contains a single, set architecture for learning both h1 and h2, 
+however, this can be extended and made modular depending on __init__ conditions.
+"""
+
+
+class Autoencoder_double(nn.Module):
+    def __init__(self, rp):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(2, 16),
+            nn.Softplus(),
+            nn.Linear(16, 16),
+            nn.Softplus(),
+            nn.Linear(16, 2)
+        )
+        
+        
+        self.decoder = nn.Sequential(
+            nn.Linear(2, 16),
+            nn.Softplus(),
+            nn.Linear(16, 16),
+            nn.Softplus(),
+            nn.Linear(16, 2)
+        )
+        
+        self.rp = rp
+        
+    def encoder_theta_1_ana(self, q):
+        theta_1 = transforms.analytic_theta_1(self.rp, q).unsqueeze(0)
+        return theta_1, theta_1
+    
+    #This function is not used in the forward pass, but is useful for comparing learned to analytic theta_2
+    def encoder_theta_2_ana(self, q):
+        theta_2 = transforms.analytic_theta_2(self.rp, q).unsqueeze(0)
+        return theta_2, theta_2
+    
+    def theta_ana(self, q):
+        theta_ana = torch.vmap(transforms.analytic_theta, in_dims=(None, 0))(self.rp, q)
+        #theta_1_ana, _ = torch.vmap(self.encoder_theta_1_ana)(q)
+        #theta_2_ana, _ = torch.vmap(self.encoder_theta_2_ana)(q)
+        #theta_ana = torch.cat((theta_1_ana, theta_2_ana), dim=1)
+        return theta_ana
+
+    def encoder_nn(self, q):
+        theta = self.encoder(q)
+        return theta, theta
+    
+    def decoder_nn(self, theta):
+        q_hat = self.decoder(theta)
+        return q_hat, q_hat
+    
+    def jacobian_enc(self, q):
+        J_h, theta = torch.vmap(torch.func.jacfwd(self.encoder_nn, has_aux=True))(q)
+        return J_h
+
+    def jacobian_dec(self, theta):
+        J_h_dec, q_hat = torch.vmap(torch.func.jacfwd(self.decoder_nn, has_aux=True))(theta)
+        return J_h_dec
+    
+    def forward(self, q):
+        
+        J_h_1_ana, theta_1_ana = torch.vmap(torch.func.jacfwd(self.encoder_theta_1_ana, has_aux=True))(q)
+        J_h_2_ana, theta_2_ana = torch.vmap(torch.func.jacfwd(self.encoder_theta_2_ana, has_aux=True))(q)
+        J_h_ana = torch.cat((J_h_1_ana, J_h_2_ana), dim=1).float()
+        
+        
+        J_h, theta = torch.vmap(torch.func.jacfwd(self.encoder_nn, has_aux=True))(q)
+
+        J_h_dec, q_hat = torch.vmap(torch.func.jacfwd(self.decoder_nn, has_aux=True))(theta)
+
+        return(theta, J_h, q_hat, J_h_dec, J_h_ana)
+    
+
+class Analytic_transformer():
+
+    """
+    This class follows the same notation as the Autoencoders, but provides the analytic solution instead. 
+    """
+
+    def __init__(self, rp):
+        self.rp = rp
+
+
+    def encoder(self, q):
+        theta_ana = torch.vmap(transforms.analytic_theta, in_dims=(None, 0))(self.rp, q)
+        #theta_1_ana, _ = torch.vmap(self.encoder_theta_1_ana)(q)
+        #theta_2_ana, _ = torch.vmap(self.encoder_theta_2_ana)(q)
+        #theta_ana = torch.cat((theta_1_ana, theta_2_ana), dim=1)
+        return theta_ana
+    
+    def decoder(self, theta):
+        q_cw, q_ccw = torch.vmap(transforms.analytic_inverse, in_dims=(None, 0))(self.rp, theta)
+        return q_ccw#, q_ccw
+    
+    def encoder_theta_1_ana(self, q):
+        theta_1 = transforms.analytic_theta_1(self.rp, q).unsqueeze(0)
+        return theta_1, theta_1
+    
+    #This function is not used in the forward pass, but is useful for comparing learned to analytic theta_2
+    def encoder_theta_2_ana(self, q):
+        theta_2 = transforms.analytic_theta_2(self.rp, q).unsqueeze(0)
+        return theta_2, theta_2
+    
+    def theta_ana(self, q):
+        theta_ana = torch.vmap(transforms.analytic_theta, in_dims=(None, 0))(self.rp, q)
+        #theta_1_ana, _ = torch.vmap(self.encoder_theta_1_ana)(q)
+        #theta_2_ana, _ = torch.vmap(self.encoder_theta_2_ana)(q)
+        #theta_ana = torch.cat((theta_1_ana, theta_2_ana), dim=1)
+        return theta_ana
+
+    def encoder_nn(self, q):
+        theta = self.encoder(q)
+        return theta, theta
+    
+    def decoder_nn(self, theta):
+        q_hat = self.decoder(theta)
+        return q_hat, q_hat
+
+    def jacobian_enc(self, q):
+        J_h, theta = torch.func.jacfwd(self.encoder_nn, has_aux=True)(q)
+        return J_h.squeeze(0).squeeze(1)
+    
+    def jacobian_dec(self, theta):
+        J_h_dec, q_hat = torch.func.jacfwd(self.decoder_nn, has_aux=True)(theta)
+        return J_h_dec.squeeze(0).squeeze(1)
+    
+    def forward(self, q):
+
+        J_h_ana = self.jacobian_enc(q)
+
+
+        J_h, theta = torch.vmap(torch.func.jacfwd(self.encoder_nn, has_aux=True))(q)
+        J_h_dec, q_hat = torch.vmap(torch.func.jacfwd(self.decoder_nn, has_aux=True))(theta)
+
+        return theta, J_h, q_hat, J_h_dec, J_h_ana
