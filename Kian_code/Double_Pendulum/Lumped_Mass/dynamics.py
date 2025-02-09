6e0d167bde4d24a8d20a42c47b69c93090579c32
kianheus <Kianheus@gmail.com>
2025-02-09 22:12:43 +0100

    Change indexing and include second mass in dynamics calculation

--- Kian_code/Double_Pendulum/Lumped_Mass/dynamics.py
+++ Kian_code/Double_Pendulum/Lumped_Mass/dynamics.py
@@ -19,24 +19,25 @@ def dynamical_matrices(rp: dict, q: Tensor, q_d: Tensor) -> Tuple[Tensor, Tensor
         C_q: coriolis and centrifugal matrix of shape (2, 2)
         G_q: gravitational matrix of shape (2, )     
     """
-    c1 = torch.cos(q[0]).unsqueeze(0)
-    c2 = torch.cos(q[1]).unsqueeze(0)
-    s12 = torch.sin(q[0]-q[1]).unsqueeze(0)
-    c12 = torch.cos(q[0]-q[1]).unsqueeze(0)
+
+    c1 = torch.cos(q[0, 0]).unsqueeze(0)
+    c2 = torch.cos(q[0, 1]).unsqueeze(0)
+    s12 = torch.sin(q[0, 0]-q[0, 1]).unsqueeze(0)
+    c12 = torch.cos(q[0, 0]-q[0, 1]).unsqueeze(0)
     
-    M_q_11 = torch.tensor([rp["l1"]**2 * rp["m"]]).to(device) 
-    M_q_1 = torch.cat((M_q_11, (rp["l1"] * rp["l2"] * rp["m"] * c12).to(device)), dim = 0).unsqueeze(0)
-    M_q_22 = torch.tensor([rp["l2"]**2 * rp["m"]]).to(device) 
-    M_q_2 = torch.cat(((rp["l1"] * rp["l2"] * rp["m"] * c12).to(device), M_q_22), dim = 0).unsqueeze(0)
+    M_q_11 = torch.tensor([rp["l1"]**2 * (rp["m1"] + rp["m2"])]).to(device) 
+    M_q_1 = torch.cat((M_q_11, (rp["l1"] * rp["l2"] * rp["m2"] * c12).to(device)), dim = 0).unsqueeze(0)
+    M_q_22 = torch.tensor([rp["l2"]**2 * rp["m2"]]).to(device) 
+    M_q_2 = torch.cat(((rp["l1"] * rp["l2"] * rp["m2"] * c12).to(device), M_q_22), dim = 0).unsqueeze(0)
     M_q = torch.cat((M_q_1, M_q_2), dim = 0)
         
     C_q_11 = torch.tensor([0]).to(device) 
-    C_q_1 = torch.cat((C_q_11, (rp["l1"] * rp["l2"] * rp["m"] * q_d[1] * s12).to(device)), dim=0).unsqueeze(0)
-    C_q_2 = torch.cat(((-rp["l1"] * rp["l2"] * rp["m"] * q_d[0] * s12).to(device), C_q_11), dim=0).unsqueeze(0)
+    C_q_1 = torch.cat((C_q_11, (rp["l1"] * rp["l2"] * rp["m2"] * q_d[0, 1] * s12).to(device)), dim=0).unsqueeze(0)
+    C_q_2 = torch.cat(((-rp["l1"] * rp["l2"] * rp["m2"] * q_d[0, 0] * s12).to(device), C_q_11), dim=0).unsqueeze(0)
     C_q = torch.cat((C_q_1, C_q_2), dim = 0)
     
-    G_q_1 = torch.tensor([rp["g"] * rp["l1"] * rp["m"]]).unsqueeze(0).to(device)  * c1.to(device)
-    G_q_2 = torch.tensor([rp["g"] * rp["l2"] * rp["m"]]).unsqueeze(0).to(device)  * c2.to(device)
+    G_q_1 = torch.tensor([rp["g"] * rp["l1"] * (rp["m1"] + rp["m2"])]).unsqueeze(0).to(device)  * c1.to(device)
+    G_q_2 = torch.tensor([rp["g"] * rp["l2"] * rp["m2"]]).unsqueeze(0).to(device)  * c2.to(device)
     G_q = torch.cat((G_q_1, G_q_2), dim = 0)
 
 
@@ -56,12 +57,11 @@ def input_matrix(rp: dict, q: Tensor) -> Tensor:
         A_q: input matrix of shape (2, )   
     """
 
-    Rx = rp["xa"] - rp["l1"] * torch.cos(q[0]) - rp["l2"] * torch.cos(q[1])
-    Ry = rp["ya"] - rp["l1"] * torch.sin(q[0]) - rp["l2"] * torch.sin(q[1])
+    Rx = rp["xa"] - rp["l1"] * torch.cos(q[0, 0]) - rp["l2"] * torch.cos(q[0, 1])
+    Ry = rp["ya"] - rp["l1"] * torch.sin(q[0, 0]) - rp["l2"] * torch.sin(q[0, 1])
 
-    A_q1 = ( (rp["l1"] * torch.sin(q[0]) * Rx - rp["l1"] * torch.cos(q[0]) * Ry) / torch.sqrt(Rx**2 + Ry**2) ).unsqueeze(0).unsqueeze(0)
-    A_q2 = ( (rp["l2"] * torch.sin(q[1]) * Rx - rp["l2"] * torch.cos(q[1]) * Ry) / torch.sqrt(Rx**2 + Ry**2) ).unsqueeze(0).unsqueeze(0)
-
+    A_q1 = ( (rp["l1"] * torch.sin(q[0, 0]) * Rx - rp["l1"] * torch.cos(q[0, 0]) * Ry) / torch.sqrt(Rx**2 + Ry**2) ).unsqueeze(0).unsqueeze(0)
+    A_q2 = ( (rp["l2"] * torch.sin(q[0, 1]) * Rx - rp["l2"] * torch.cos(q[0, 1]) * Ry) / torch.sqrt(Rx**2 + Ry**2) ).unsqueeze(0).unsqueeze(0)
     A_q = torch.cat((A_q1, A_q2), dim = 0)
 
     return A_q
