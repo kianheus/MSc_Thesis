4668cfb289e0db34b6b050f2b14ba0e1a53401d5
kianheus <Kianheus@gmail.com>
2025-03-11 17:01:52 +0100

    Add small mass to first link, and change actuator coordinates

--- Kian_code/Double_Pendulum/Lumped_Mass/robot_parameters.py
+++ Kian_code/Double_Pendulum/Lumped_Mass/robot_parameters.py
@@ -12,8 +12,9 @@ LUMPED_PARAMETERS = dict(
 LUMPED_PARAMETERS = dict(
     l1 = 3.1, #Length of link 1 [m]
     l2 = 2., #Length of link 2 [m]
-    m = 3., #System mass [kg] (attached at the end of link 2)
+    m1 = 0.1, #System mass [kg] (attached at the elbow)
+    m2 = 3., #System mass [kg] (attached at the end of link 2)
     g = 9.81, #Gravitational acceleration [m/s^2]
-    xa = 5., #actuator rope attachment point x-location [m]
-    ya = 2., #actuator rope attachment point y-location [m]
+    xa = 2., #actuator rope attachment point x-location [m]
+    ya = 5, #actuator rope attachment point y-location [m]
 )
