Fix spring force calculation

Kian_code/Double_Pendulum/dynamics.py
@@ -45,8 +45,8 @@ def dynamical_matrices(rp: dict, q: Tensor, q_d: Tensor) -> Tuple[Tensor, Tensor
 
 def add_spring_force_G_q(rp: dict, q: Tensor, G_q, k_spring):
 
-    K_q = torch.tensor([[k_spring, -k_spring], 
-                        [-k_spring, k_spring]]).to(device)
+    K_q = torch.tensor([[-k_spring, k_spring], 
+                        [k_spring, -k_spring]]).to(device)
     
     G_q += K_q @ q.T
 