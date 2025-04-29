0d6c2b4d46528757a6e3b5d61449fd0394cfa3e1
kianheus <Kianheus@gmail.com>
2025-04-29 11:44:36 +0200

    Add SEA learning files in separate folder

---
+++ Kian_code/Series_Elastic_Actuator/Learning/autoencoders_SEA.py
@@ -0,0 +1,166 @@
+import torch
+import torch.nn as nn
+from functools import partial
+
+import Series_Elastic_Actuator.transforms_SEA as transforms_SEA
+
+"""
+This file contains the architectures of a number of Autoencoders used in my thesis. 
+Currently it only contains a single, set architecture for learning both h1 and h2, 
+however, this can be extended and made modular depending on __init__ conditions.
+"""
+
+
+class Autoencoder_double(nn.Module):
+    def __init__(self, rp):
+        super().__init__()
+        self.enc = nn.Sequential(
+            nn.Linear(2, 2),
+            #nn.Softplus(),
+            #nn.Linear(4, 4),
+            nn.Softplus(),
+            nn.Linear(2, 2)
+        )
+        
+        
+        self.dec = nn.Sequential(
+            nn.Linear(2, 2),
+            #nn.Softplus(),
+            #nn.Linear(4, 4),
+            nn.Softplus(),
+            nn.Linear(2, 2)
+        )
+        
+        self.rp = rp
+        
+    def encoder_theta_0_ana(self, q):
+        theta_0 = transforms_SEA.analytic_theta_0(self.rp, q).unsqueeze(0)
+        return theta_0, theta_0
+    
+    #This function is not used in the forward pass, but is useful for comparing learned to analytic theta_1
+    def encoder_theta_1_ana(self, q):
+        theta_1 = transforms_SEA.analytic_theta_1(self.rp, q).unsqueeze(0)
+        return theta_1, theta_1
+    
+    def theta_ana(self, q):
+        theta_ana = torch.vmap(transforms_SEA.analytic_theta, in_dims=(None, 0))(self.rp, q)
+        return theta_ana
+    
+    def encoder(self, q):
+        theta = self.enc(q)
+        return theta
+    
+    def decoder(self, theta, clockwise=None):
+        q_hat = self.dec(theta)
+        return q_hat
+
+    def encoder_vmap(self, q):
+        theta = self.encoder(q)
+        return theta
+    
+    def decoder_vmap(self, theta, clockwise=None):
+        q_hat = self.decoder(theta)
+        return q_hat
+    
+    def jacobian_enc(self, q, clockwise=None):
+        J_h = torch.vmap(torch.func.jacfwd(self.encoder_vmap, has_aux=False))(q)
+        return J_h.squeeze(0)
+
+    def jacobian_dec(self, theta, clockwise=None):
+        J_h_dec = torch.vmap(torch.func.jacfwd(self.decoder_vmap, has_aux=False))(theta)
+        return J_h_dec.squeeze(0)
+    
+    def forward(self, q):
+        
+        J_h_0_ana, theta_0_ana = torch.vmap(torch.func.jacfwd(self.encoder_theta_0_ana, has_aux=True))(q)
+        J_h_1_ana, theta_1_ana = torch.vmap(torch.func.jacfwd(self.encoder_theta_1_ana, has_aux=True))(q)
+        J_h_ana = torch.cat((J_h_0_ana, J_h_1_ana), dim=1).float()
+        
+        theta = self.encoder(q)
+        J_h = self.jacobian_enc(q) 
+        #J_h = torch.vmap(torch.func.jacfwd(self.encoder_vmap, has_aux=False))(q)
+        q_hat = self.decoder(theta)
+        J_h_dec = self.jacobian_dec(theta)
+        #J_h_dec, q_hat = torch.vmap(torch.func.jacfwd(self.decoder_vmap, has_aux=True))(theta)
+
+        return(theta, J_h, q_hat, J_h_dec, J_h_ana)
+    
+
+class Analytic_transformer():
+
+    """
+    This class follows the same notation as the Autoencoders, but provides the analytic solution instead. 
+    """
+
+    def __init__(self, rp):
+        self.rp = rp
+
+
+    def encoder(self, q):
+        theta_ana = transforms_SEA.analytic_theta(self.rp, q)
+        return theta_ana
+    
+    def decoder(self, theta, clockwise):
+        q_ccw, q_cw = torch.vmap(transforms_SEA.analytic_inverse, in_dims=(None, 0))(self.rp, theta)
+        if clockwise:
+            return q_cw
+        else:
+            return q_ccw
+        
+    def decoder_cw(self, theta):
+        q_ccw, q_cw = transforms_SEA.analytic_inverse(self.rp, theta)
+        return q_cw
+    
+    def decoder_ccw(self, theta):
+        q_ccw, q_cw = transforms_SEA.analytic_inverse(self.rp, theta)
+        return q_ccw
+    
+    def encoder_theta_0_ana(self, q):
+        theta_0 = transforms_SEA.analytic_theta_0(self.rp, q).unsqueeze(0)
+        return theta_0, theta_0
+    
+    #This function is not used in the forward pass, but is useful for comparing learned to analytic theta_1
+    def encoder_theta_1_ana(self, q):
+        theta_1 = transforms_SEA.analytic_theta_1(self.rp, q).unsqueeze(0)
+        return theta_1, theta_1
+    
+    def theta_ana(self, q):
+        theta_ana = torch.vmap(transforms_SEA.analytic_theta, in_dims=(None, 0))(self.rp, q)
+        return theta_ana
+
+    def encoder_vmap(self, q):
+        theta = torch.vmap(self.encoder)(q)
+        return theta
+    
+    def decoder_vmap(self, theta, clockwise):
+        if clockwise:
+            q_hat = torch.vmap(self.decoder_cw)(theta)
+            return q_hat
+        else:
+            q_hat = torch.vmap(self.decoder_ccw)(theta)
+            return q_hat
+
+    def jacobian_enc(self, q, clockwise=None):
+        J_h = torch.vmap(torch.func.jacfwd(self.encoder, has_aux=False))(q)
+        return J_h.squeeze(0).float()
+
+    def jacobian_dec(self, theta, clockwise=None):
+        if clockwise:
+            J_h_dec = torch.vmap(torch.func.jacfwd(self.decoder_cw, has_aux=False))(theta)
+        else:
+            J_h_dec = torch.vmap(torch.func.jacfwd(self.decoder_ccw, has_aux=False))(theta)
+        return J_h_dec.squeeze(0).float()
+    
+
+    def forward(self, q, clockwise):
+
+        J_h_ana = self.jacobian_enc(q)
+
+        theta = self.theta_ana(q)
+        q_hat = self.decoder_vmap(theta, clockwise)
+        J_h = torch.vmap(torch.func.jacfwd(self.encoder_nn, has_aux=True))(q)
+        J_h_dec, q_hat = torch.vmap(torch.func.jacfwd(self.decoder_nn_cw, has_aux=True))(theta)
+        print("Only taking cw decoder, are you sure you want this?")
+        # Change depending on your use case
+
+        return theta, J_h, q_hat, J_h_dec, J_h_ana
---
+++ Kian_code/Series_Elastic_Actuator/Learning/training_data_SEA.py
@@ -0,0 +1,17 @@
+import torch
+
+# Number of samples
+n_samples = 3000
+
+# Data range
+q0_low  = -torch.pi
+q0_high = torch.pi
+q1_low  = -torch.pi
+q1_high = torch.pi
+
+# Generate uniformly distributed points for q1 and q2
+q0 = q0_low + (q0_high - q0_low) * torch.rand(n_samples)
+q1 = q1_low + (q1_high - q1_low) * torch.rand(n_samples)
+
+# Stack q1 and q2 to get the 2D coordinates
+points = torch.stack([q0, q1], axis=1)
