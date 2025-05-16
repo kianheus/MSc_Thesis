a48aef13c406e1401edfed906eb380f49b29eeb8
kianheus <Kianheus@gmail.com>
2025-05-16 16:55:57 +0200

    remove SYMMETRY params

--- Kian_code/Double_Pendulum/robot_parameters.py
+++ Kian_code/Double_Pendulum/robot_parameters.py
@@ -7,13 +7,3 @@ LUMPED_PARAMETERS = dict(
     xa = 2., #actuator rope attachment point x-location [m]
     ya = 5, #actuator rope attachment point y-location [m]
 )
-
-LUMPED_PARAMETERS_SYMMETRY = dict(
-    l0 = 3.1, #Length of link 1 [m]
-    l1 = 3.1, #Length of link 2 [m]
-    m0 = 0.1, #System mass [kg] (attached at the elbow)
-    m1 = 3., #System mass [kg] (attached at the end of link 2)
-    g = 9.81, #Gravitational acceleration [m/s^2]
-    xa = 2., #actuator rope attachment point x-location [m]
-    ya = 5, #actuator rope attachment point y-location [m]
-)
